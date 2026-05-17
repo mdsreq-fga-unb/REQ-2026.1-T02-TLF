0a13b31595d46498d801de7dcd6e25a1378da6d2
Gabro-MO <gabriel1990mota@gmail.com>
2026-05-17 14:09:16 -0300

    fix(login/register): atualização da paginas de login e cadastro para a versão certa

--- apps/mobile/app/(auth)/_layout.tsx
+++ apps/mobile/app/(auth)/_layout.tsx
@@ -1,46 +1,10 @@
-import { useThemeColor } from '@/hooks/useThemeColor'
-import { Tabs } from 'expo-router'
-import Ionicons from '@expo/vector-icons/Ionicons'
+import { Stack } from 'expo-router'
 
-export default function TabsLayout() {
+export default function AuthLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-        tabBarStyle: {
-          backgroundColor: useThemeColor().background,
-          borderTopColor: useThemeColor().gray,
-        },
-        tabBarActiveTintColor: useThemeColor().graySecondary,
-        tabBarInactiveTintColor: useThemeColor().gray,
-      }}
-    >
-      <Tabs.Screen
-        name="login"
-        options={{
-          title: 'Entrar',
-          tabBarIcon: ({ focused }) => (
-            <Ionicons
-              size={30}
-              name={focused ? 'log-in' : 'log-in-outline'}
-              color={focused ? useThemeColor().graySecondary : useThemeColor().gray}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="register"
-        options={{
-          title: 'Cadastro',
-          tabBarIcon: ({ focused }) => (
-            <Ionicons
-              size={30}
-              name={focused ? 'book' : 'book-outline'}
-              color={focused ? useThemeColor().graySecondary : useThemeColor().gray}
-            />
-          ),
-        }}
-      />
-    </Tabs>
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="login" />
+      <Stack.Screen name="register" />
+    </Stack>
   )
 }

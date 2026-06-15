e1348b3ac6b52f66e31678ea6d40a7639136ae5d
Gabro-MO <gabriel1990mota@gmail.com>
2026-06-15 15:33:24 -0300

    fix(menu): correção de componentes com o mesmo nome

--- apps/mobile/app/(tabs)/menu.tsx
+++ apps/mobile/app/(tabs)/menu.tsx
@@ -2,14 +2,14 @@ import { ThemedBackground } from '@/components/ui/ThemedBackground'
 import { ThemedListItem } from '@/components/ui/ThemedListItem'
 import { FileText, ChartColumnStacked } from 'lucide-react-native'
 import { router } from 'expo-router'
-import { ThemedHeader } from '@/components/ui/ThemedHeader'
+import { ThemedSimpleHeader } from '@/components/ui/ThemedSimpleHeader'
 import { ThemedText } from '@/components/ui/ThemedText'
 import { ThemedContainer } from '@/components/ui/ThemedContainer'
 
 export default function BudgetsScreen() {
   return (
     <ThemedBackground>
-      <ThemedHeader text="Menu" />
+      <ThemedSimpleHeader text="Menu" />
       <ThemedContainer variant="transparent" style={{ alignItems: 'flex-start' }}>
         <ThemedText children text="Funções" variant="label" tone="muted" />
         <ThemedListItem

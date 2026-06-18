de061590f3175e5933246ca227bb6bfb9b29a9f9
EngDann <danilodemeloribeiro0@gmail.com>
2026-06-18 08:45:41 -0300

    feat: extend database schema and models to include categories, subcategories, institutions, accounts, invoices, and recurrences

--- apps/mobile/src/services/database/index.ts
+++ apps/mobile/src/services/database/index.ts
@@ -3,6 +3,13 @@ import SQLiteAdapter from '@nozbe/watermelondb/adapters/sqlite'
 import { schema } from './schema'
 import { Transaction } from './models/transaction'
 import { Notification } from './models/notification'
+import { Category } from './models/category'
+import { SubCategory } from './models/subCategory'
+import { Institution } from './models/institution'
+import { Account } from './models/account'
+import { Invoice } from './models/invoice'
+import { Recurrence } from './models/recurrece'
+import { Budget } from './models/budget'
 import migrations from './migration'
 
 const adapter = new SQLiteAdapter({
@@ -15,5 +22,15 @@ const adapter = new SQLiteAdapter({
 
 export const database = new Database({
   adapter,
-  modelClasses: [Transaction, Notification],
+  modelClasses: [
+    Category,
+    SubCategory,
+    Institution,
+    Budget,
+    Account,
+    Invoice,
+    Recurrence,
+    Transaction,
+    Notification,
+  ],
 })
